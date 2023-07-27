e9918f04d4534551d44d613beb8d561b2e41ff97
PaPa003 <102209815+PaPa003@users.noreply.github.com>
2023-07-27 15:03:18 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from modeling import predict_input
+import streamlit as st
+from PIL import Image
+
+
+##### CONFIG
+
+# page config
+st.set_page_config(page_title            = "Fake review prediction", 
+                   page_icon             = ":pencil:", 
+                   layout                = "centered", 
+                   initial_sidebar_state = "collapsed", 
+                   menu_items            = None)
+
+
+
+
+# creating a function for prediction
+def prediction(input_data):
+
+    # loading the model
+    model = predict_input.get_prediction(input_data)
+    print(model)
+
+    if (model[0]==0):
+        return 'The Review is Real'
+
+    else:
+        return 'The Review is Fake'
+
+
+
+def main():
+
+    # setting the title
+    st.title('Fake Review Prediction')
+
+    # image cover
+    image = Image.open('image.jpg')
+    st.image(image)
+
+    # getting the input from the user
+    Review = st.text_input('Product Review')
+
+    # prediction code
+    result = ''
+
+    # creating a button for prediction
+    if st.button('Submit'):
+        result = prediction(Review)
+
+    st.success(result)
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
---
+++ modeling.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from joblib import Parallel, delayed
+import joblib
+
+
+class Preprocessing:
+    def stemmed(content):
+        stemmer = PorterStemmer()
+        stemmed_content = re.sub('[^a-zA-Z]', ' ', content)
+        stemmed_content = stemmed_content.lower()
+        stemmed_content = stemmed_content.split()
+        stemmed_content = [stemmer.stem(word) for word in stemmed_content if not word in stopwords.words('english')]
+        stemmed_content = ' '.join(stemmed_content)
+        return stemmed_content
+    
+    def preprocess_data(y):
+        pre = [Preprocessing.stemmed(y)]
+        # Load the model from the file
+        vec = joblib.load('vectorizer.pkl')
+        pre_num = vec.transform(pre)
+        return pre_num
+
+class predict_input:
+    def get_prediction(input_data):
+        input = Preprocessing.preprocess_data(input_data)
+
+        # Load the model from the file
+        classifier = joblib.load('model.pkl')
+        
+        # Use the loaded model to make predictions
+        prediction = classifier.predict(input)
+
+        return prediction
+
+
